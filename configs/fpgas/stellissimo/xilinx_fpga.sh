10945efc3ec8c88f6b79928c37c2df6a6f76148e
Maximilian Koschay <mkoschay@web.de>
2020-07-04 19:04:08 +0200

    Fixed pulp-rt linker selection

--- configs/fpgas/stellissimo/xilinx_fpga.sh
+++ configs/fpgas/stellissimo/xilinx_fpga.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PULP_CURRENT_CONFIG=pulpissimo@config_file=fpgas/stellissimo/xilinx_fpga.json
+export PULP_CURRENT_CONFIG=stellissimo@config_file=fpgas/stellissimo/xilinx_fpga.json
 
 export PULP_CURRENT_CONFIG_ARGS=platform=fpga
